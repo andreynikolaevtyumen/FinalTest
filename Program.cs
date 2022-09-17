294ceaec8845728e611d0afdcadaa45a0409edfd
andreynikolaevtyumen <andreynikolaev66@gmail.com>
2022-09-17 16:39:33 +0500

    Прописан метод CheckArray для отбора элементов нового массива

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿string[] array = new string[] { "hello", "2", "world", ";-)", "!!!", "maximum" };
 string[] newarray = new string[array.Length];
 int n = 0;
+
+void CheckArray()
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        if(array[i].Length <=3)
+        {
+            newarray[n] = array[i];
+            n++;
+        }
+    }
+}
